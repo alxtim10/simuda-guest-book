3c16cc79db7dafadf2c92c7c5763979a89e91f42
Alexander Tim <alxtim10@gmail.com>
2023-09-14 20:21:51 +0700

    update pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/simuda-guest-book" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="dashboard" element={<Dashboard />} />
     </Routes>
   );
